32f1587e249ad7bea7564428fb8f1dbf05b8d518
Vidhu Appalaraju <vidhuappalaraju@gmail.com>
2018-09-11 19:09:25 -0500

    Added detailsViewController

--- flix_application/ViewControllers/NowPlayingViewController.swift
+++ flix_application/ViewControllers/NowPlayingViewController.swift
@@ -87,7 +87,14 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
         return cell
     }
     
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell){
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            detailViewController.movie = movie
+        }
+    }
     
     
     override func didReceiveMemoryWarning() {
